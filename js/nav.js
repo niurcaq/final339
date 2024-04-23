b130b881b055895e8681be40ae6a9d4c2f922d0c
Niurca Quirarte <niurcaq@umich.edu>
2024-04-23 01:26:28 -0400

    done

--- js/nav.js
+++ js/nav.js
@@ -238,3 +238,111 @@ function closeKG() {
         document.getElementById("KGnav").style.height = "0";
     }
 }
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var hiddenD = document.getElementById('hideD');
+    var shownD = document.getElementById('showD');
+
+    shownD.addEventListener('click', function() {
+    if (hiddenD.style.display === 'grid') {
+        hiddenD.style.display = 'none';
+        shownD.innerHTML = "&or;"
+    } else {
+        hiddenD.style.display = 'grid';
+        shownD.innerHTML = "&and;";
+    }
+    });
+
+    var hiddenE = document.getElementById('hideE');
+    var shownE = document.getElementById('showE');
+
+    shownE.addEventListener('click', function() {
+    if (hiddenE.style.display === 'grid') {
+        hiddenE.style.display = 'none';
+        shownE.innerHTML = "&or;"
+    } else {
+        hiddenE.style.display = 'grid';
+        shownE.innerHTML = "&and;";
+    }
+    });
+
+    var hiddenR = document.getElementById('hideR');
+    var shownR = document.getElementById('showR');
+
+    shownR.addEventListener('click', function() {
+    if (hiddenR.style.display === 'grid') {
+        hiddenR.style.display = 'none';
+        shownR.innerHTML = "&or;"
+    } else {
+        hiddenR.style.display = 'grid';
+        shownR.innerHTML = "&and;";
+    }
+    });
+
+    var hiddenN = document.getElementById('hideN');
+    var shownN = document.getElementById('showN');
+
+    shownN.addEventListener('click', function() {
+    if (hiddenN.style.display === 'grid') {
+        hiddenN.style.display = 'none';
+        shownN.innerHTML = "&or;"
+    } else {
+        hiddenN.style.display = 'grid';
+        shownN.innerHTML = "&and;";
+    }
+    });
+
+    var hiddenHH = document.getElementById('hideHH');
+    var shownHH = document.getElementById('showHH');
+
+    shownHH.addEventListener('click', function() {
+    if (hiddenHH.style.display === 'grid') {
+        hiddenHH.style.display = 'none';
+        shownHH.innerHTML = "&or;"
+    } else {
+        hiddenHH.style.display = 'grid';
+        shownHH.innerHTML = "&and;";
+    }
+    });
+
+    var hiddenMM = document.getElementById('hideMM');
+    var shownMM = document.getElementById('showMM');
+
+    shownMM.addEventListener('click', function() {
+    if (hiddenMM.style.display === 'grid') {
+        hiddenMM.style.display = 'none';
+        // $("hiddenMMshow").fadeIn(1500);
+        shownMM.innerHTML = "&or;"
+    } else {
+        hiddenMM.style.display = 'grid';
+        shownMM.innerHTML = "&and;";
+    }
+    });
+
+    var hiddenP = document.getElementById('hideP');
+    var shownP = document.getElementById('showP');
+
+    shownP.addEventListener('click', function() {
+    if (hiddenP.style.display === 'grid') {
+        hiddenP.style.display = 'none';
+        shownP.innerHTML = "&or;"
+    } else {
+        hiddenP.style.display = 'grid';
+        shownP.innerHTML = "&and;";
+    }
+    });
+
+    var hiddenUL = document.getElementById('hideUL');
+    var shownUL = document.getElementById('showUL');
+
+    shownUL.addEventListener('click', function() {
+    if (hiddenUL.style.display === 'grid') {
+        hiddenUL.style.display = 'none';
+        shownUL.innerHTML = "&or;"
+    } else {
+        hiddenUL.style.display = 'grid';
+        shownUL.innerHTML = "&and;";
+    }
+    });
+});
